1dc7f110288fed409319e27190d33561c1118c6f
Sam Tobin <smtobin@wisc.edu>
2025-10-07 15:25:57 -0400

    Added initial angle config parameter to XPBDPendulum

--- include/config/XPBDPendulumConfig.hpp
+++ include/config/XPBDPendulumConfig.hpp
@@ -23,11 +23,15 @@ public:
         : XPBDObjectConfig(node)
     {
         _extractParameter("num-bodies", node, _num_bodies);
+        _extractParameter("initial-angle", node, _initial_angle);
     }
 
     int numBodies() const { return _num_bodies.value; }
+    Real initialAngle() const { return _initial_angle.value; }
+    
 private:
     ConfigParameter<int> _num_bodies = ConfigParameter<int>(2);
+    ConfigParameter<Real> _initial_angle = ConfigParameter<Real>(45);
 };
 
 } // namespace Config
--- include/simobject/group/XPBDPendulum.hpp
+++ include/simobject/group/XPBDPendulum.hpp
@@ -18,6 +18,7 @@ public:
         : XPBDObjectGroup_Base(config)
     {
         _num_bodies = config.numBodies();
+        _initial_angle = config.initialAngle();
     }
 
     virtual void setup() override
@@ -28,19 +29,29 @@ public:
         XPBDRigidBox& base = _addObject<XPBDRigidBox>(base_config);
         base.setFixed(true);
 
+        Real body_length = 1.0;
+
 
         std::vector<XPBDRigidBox>& bodies = _objects.template get<XPBDRigidBox>();
         for (int i = 0; i < _num_bodies; i++)
         {
-            Config::XPBDRigidBoxConfig box_config("box", Vec3r(0,i+0.5,0), Vec3r(0,0,0), Vec3r(0,0,0), Vec3r(0,0,0),
-                1000, Vec3r(0.1, 1, 0.1));
+            Real pos_x = (0.5 + i*body_length) * std::sin(_initial_angle * M_PI/180.0);
+            Real pos_y = (-0.5 - i*body_length) * std::cos(_initial_angle * M_PI/180.0);
+            Config::XPBDRigidBoxConfig box_config("box", Vec3r(pos_x,pos_y,0), Vec3r(0,0,_initial_angle), Vec3r(0,0,0), Vec3r(0,0,0),
+                1000, Vec3r(0.1, body_length, 0.1));
             _addObject<XPBDRigidBox>(box_config);
-            _addConstraint<Constraint::RevoluteJointConstraint>(&(bodies[i].com()), Vec3r(0,0.5,0), Mat3r::Identity(), &(bodies[i+1].com()), Vec3r(0,-0.5,0), Mat3r::Identity());
+        }
+
+        _addConstraint<Constraint::RevoluteJointConstraint>(&(bodies[0].com()), Vec3r(0,0,0), Mat3r::Identity(), &(bodies[1].com()), Vec3r(0,0.5*body_length,0), Mat3r::Identity());
+        for (int i = 1; i < _num_bodies; i++)
+        {
+            _addConstraint<Constraint::RevoluteJointConstraint>(&(bodies[i].com()), Vec3r(0,-0.5*body_length,0), Mat3r::Identity(), &(bodies[i+1].com()), Vec3r(0,0.5*body_length,0), Mat3r::Identity());
         }
     }
 
 private:
     int _num_bodies;
+    Real _initial_angle;
 
 };
 
