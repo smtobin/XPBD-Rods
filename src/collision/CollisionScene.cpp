410a1dc92e18ca7aca940aa2ae00d7def916b259
Sam Tobin <smtobin@wisc.edu>
2026-01-29 22:34:43 -0500

    Implemented box-sphere collision

--- src/collision/CollisionScene.cpp
+++ src/collision/CollisionScene.cpp
@@ -129,6 +129,68 @@ void CollisionScene::_checkCollision(CollisionScene* scene, SimObject::XPBDRigid
 void CollisionScene::_checkCollision(CollisionScene* scene, SimObject::XPBDRigidSphere* sphere, SimObject::XPBDRigidBox* box)
 {
     std::cout << "Potential collision between a sphere and a box!" << std::endl;
+    // find closest point on box to sphere center
+    // first, transform sphere center into box local frame
+    const Vec3r sphere_local = box->com().orientation.transpose() * (sphere->com().position - box->com().position);
+    Vec3r box_closest_point(
+        std::clamp(sphere_local[0], -box->size()[0]/2, box->size()[0]/2),
+        std::clamp(sphere_local[1], -box->size()[1]/2, box->size()[1]/2),
+        std::clamp(sphere_local[2], -box->size()[2]/2, box->size()[2]/2)
+    );
+
+    // vector (in box frame) from closest point to sphere center
+    const Vec3r diff = sphere_local - box_closest_point;
+    Real sq_dist = diff.squaredNorm();
+
+    Vec3r local_collision_normal;
+    if (sq_dist <= sphere->radius() * sphere->radius())
+    {
+        // collision!
+        std::cout << "\n\n\n COLLISION BETWEEN SPHERE AND BOX!\n\n\n" << std::endl;
+
+        Real dist = std::sqrt(sq_dist);
+        
+        // edge case: sphere center inside box
+        // because of the clamp operation, the distance to the closest point in the box will be 0
+        if (dist < 1e-6)
+        {
+            // find the axis with the minimum penetration
+            const Vec3r penetrations = box->size()/2 - sphere_local.cwiseAbs();
+            if (penetrations[0] < penetrations[1] && penetrations[0] < penetrations[2])
+            {
+                // minimum penetration along local x axis
+                local_collision_normal = Vec3r(sphere_local[0] > 0 ? 1 : -1, 0, 0);
+                box_closest_point[0] = box->size()[0]/2 * (sphere_local[0] > 0 ? 1 : -1); 
+            }
+            else if (penetrations[1] < penetrations[2])
+            {
+                // minimum penetration is along local y axis
+                local_collision_normal = Vec3r(0, sphere_local[1] > 0 ? 1 : -1, 0);
+                box_closest_point[1] = box->size()[1]/2 * (sphere_local[1] > 0 ? 1 : -1);
+            }
+            else
+            {
+                // minimum penetration is along local z axis
+                local_collision_normal = Vec3r(0, 0, sphere_local[2] > 0 ? 1 : -1);
+                box_closest_point[2] = box->size()[2]/2 * (sphere_local[2] > 0 ? 1 : -1);
+            }
+        }
+        else
+        {
+            // normal case: sphere center outside box
+            local_collision_normal = diff / dist;
+        }
+
+        Vec3r collision_normal = box->com().orientation * local_collision_normal;
+        Vec3r cp_sphere_local = -sphere->com().orientation.transpose() * collision_normal * sphere->radius();
+
+        // create collision constraint
+        scene->_new_collision_constraints.template emplace_back<Constraint::RigidBodyCollisionConstraint>(  
+            &box->com(), box_closest_point,
+            &sphere->com(), cp_sphere_local,
+            collision_normal
+        );
+    }
 }
 
 void CollisionScene::_checkCollision(CollisionScene* scene, SimObject::XPBDRigidSphere* sphere, SimObject::XPBDRodSegment* segment)
