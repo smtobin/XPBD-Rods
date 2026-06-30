61f8e1d60af7084aaa909e6261bcab916bbfd35b
Sam Tobin <smtobin@wisc.edu>
2026-06-30 16:21:58 -0400

    Added launch file for sim bridge node

---
+++ ros_workspace/launch/sim_bridge.launch.py
@@ -0,0 +1,59 @@
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.launch_description_sources import AnyLaunchDescriptionSource
+from launch.substitutions import PathJoinSubstitution, LaunchConfiguration, TextSubstitution
+from launch_ros.actions import Node
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, LogInfo, IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource, AnyLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+def generate_launch_description():
+    config_file_arg = DeclareLaunchArgument(
+        'config_filename',
+        default_value=TextSubstitution(text='../config/config.yaml'),
+        description='Absolute path to the simulation config file.'
+    )
+
+    # the sim_bridge node
+    sim_bridge_node = Node(
+        package='sim_bridge',
+        executable='sim_bridge_node',
+        name='sim_bridge',
+        output='screen',
+        remappings=[
+        ],
+        parameters=[
+            {"use_wall_time_for_publishing": True},   # if true, the rate of publishing will be in terms of the wall time (which may not align with simulated time)
+            {"publish_rate_hz": 10.0},      # publish rate of topics
+        ],
+        arguments=[
+            '--config-filename', LaunchConfiguration('config_filename')
+        ]
+    )
+
+    # Include rosbridge_server launch file
+    rosbridge_launch_file = PathJoinSubstitution([
+        FindPackageShare('rosbridge_server'),
+        'launch',
+        'rosbridge_websocket_launch.xml'
+    ])
+    
+    # Create the include launch description action
+    rosbridge_server = IncludeLaunchDescription(
+        AnyLaunchDescriptionSource(rosbridge_launch_file),
+        launch_arguments={
+            'delay_between_messages': '0.0',
+        }.items()
+    )
+
+    ld = LaunchDescription([
+        config_file_arg,
+        sim_bridge_node,
+        rosbridge_server
+    ])
+
+    return ld
