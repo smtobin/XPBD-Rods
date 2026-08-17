1e11649553454ea77b143da1d5504f3b18326a20
Sam Tobin <smtobin@wisc.edu>
2026-08-17 16:41:34 -0400

    Working rigid mesh orientation?

--- src/simobject/rigidbody/XPBDRigidMesh.cpp
+++ src/simobject/rigidbody/XPBDRigidMesh.cpp
@@ -61,26 +61,35 @@ XPBDRigidMesh::XPBDRigidMesh(const Config::XPBDRigidMeshConfig& config)
     Mat3r D = R.transpose() * rot_inertia * R;
     _com.Ib = D.diagonal();
 
+    std::cout << "Ib: " << _com.Ib.transpose() << std::endl;
+    std::cout << R * rot_inertia * R.transpose() << std::endl;
+
     // update the orientation to reflect the rotation required for the rotational inertia to be diagonal
-    // _com.orientation = R * _com.orientation;
-    // _com.prev_orientation = _com.orientation;
+    _com.orientation = R * _com.orientation;
+    _com.prev_orientation = _com.orientation;
 
     // compute the unoriented AABB size of the mesh for computing the updated AABB later
     SimObject::AABB aabb = _mesh.boundingBox();
     Vec3r size = aabb.max - aabb.min;
     // rotate the bounding box back to what the XPBDRigidMesh thinks is 0 rotation
-    Mat3r RT_abs = R.transpose().cwiseAbs();
-    _unoriented_size = RT_abs.col(0) * size[0] + RT_abs.col(1) * size[1] + RT_abs.col(2) * size[2];
+    // _unoriented_size = R.transpose().cwiseAbs() * size;
+    _unoriented_size = size;
+
+    std::cout << "Mesh bbox: " << aabb.min.transpose() << " to " << aabb.max.transpose() << std::endl;
+    std::cout << "Unoriented size: " << _unoriented_size.transpose() << std::endl;
+    AABB bbox = boundingBox();
+    std::cout << "Oriented bbox: " << bbox.min.transpose() << " to " << bbox.max.transpose() << std::endl;
     
 }
 
 AABB XPBDRigidMesh::boundingBox() const
 {
-    Mat3r R_abs = _com.orientation.cwiseAbs();
-    Vec3r AABB_size = R_abs.col(0) * _unoriented_size[0] + R_abs.col(1) * _unoriented_size[1] + R_abs.col(2) * _unoriented_size[2];
+    Vec3r size = _com.orientation.cwiseAbs() * _unoriented_size;
+
     AABB bbox;
-    bbox.min = _com.position - AABB_size/2;
-    bbox.max = _com.position + AABB_size/2;
+    bbox.min = _com.position - 0.5 * size;
+    bbox.max = _com.position + 0.5 * size;
+
     return bbox;
 }
 
